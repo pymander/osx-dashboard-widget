c49e2b9369bbf120a296a1945cac9b93e64d8f5e
Taavi Burns <taavi@freshbooks.com>
2010-03-15 17:20:17 -0400

    Update to v1.0.5, and allow '-' in domain names.

--- FreshBooks Time Tracker.wdgtproj/project.wdgt/FreshBooks Time Tracker.js
+++ FreshBooks Time Tracker.wdgtproj/project.wdgt/FreshBooks Time Tracker.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2008 2ndSite Inc. (www.freshbooks.com)
 // Licenced under the MIT license (see MITLICENSE.txt).
 
-com.freshbooks.api.UA = "FreshBooks Time Tracker Widget 1.0.4";
+com.freshbooks.api.UA = "FreshBooks Time Tracker Widget 1.0.5";
 
 debug = false;
 xmlTimeout = 15 * 1000; // Wait this many milliseconds for FreshBooks to reply before cancelling an XML call.
@@ -240,7 +240,7 @@ function clickedStartStop(event)
 }
 
 function validateSitename(sitename) {
-	if (sitename.length == 0 || sitename.search(/[^a-zA-Z0-9]/) > -1) return false;
+	if (sitename.length == 0 || sitename.search(/[^a-zA-Z0-9-]/) > -1) return false;
 	return true;
 }
 function validateToken(token) {
